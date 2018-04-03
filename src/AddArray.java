f9594fa0204f6ad5ab6d58d4dace19bb8f669e24
Gathers <cmgathers.2017@mymail.jcsu.edu>
2018-04-03 13:09:44 -0400

    first commit

---
+++ src/AddArray.java
@@ -0,0 +1,22 @@
+
+public class AddArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[]numbers={23,35,21};
+		int[]numbers1={30,0,11};
+	    int[]sum=new int [3];
+	    for (int z=0; z<sum.length;z++){
+	    	sum[z]=(numbers[z]+numbers1[z]);
+	    	System.out.println(sum[z]);
+	    
+	    }
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/AddArray2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class AddArray2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scan=new Scanner (System.in);
+//asking for the array
+System.out.println("Enter 3 integers");
+int[]numbers=new int[3];
+System.out.println("Enter 3 integers");
+int[]numbers1=new int[3];
+
+int[]sum=new int [3];
+for (int z=1; z<sum.length;z++){
+	sum[z]=(numbers[z]+numbers1[z]);
+	System.out.println(sum[z]);
+	
+	
+}
+	
+	
+	}
+	
+	
+}
